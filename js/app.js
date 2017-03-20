a43cfb351ec2cac6f780835a813171557d2f7bd2
NielsGamborg <nielsgpost@gmail.com>
2017-03-20 20:39:54 +0100

    Start metronome with enter, code clean up, styling

--- js/app.js
+++ js/app.js
@@ -5,41 +5,42 @@ Vue.filter('showBeat', function(value) {
 })
 
 Vue.component('metronome-box', {
-    props: ['startMetronome', 'stopMetronome', 'changeSound', 'sounds', 'isPlaying', 'beats', 'counter', 'beatObj'],
+    props: ['startMetronome', 'stopMetronome', 'updateMetronome', 'sounds', 'isPlaying', 'beats', 'counter', 'beatObj'],
     template: `
     <div id="metronomeBox">
-        <input type="text" v-model="bpm" :value="beats">
-        <button id="startButton" v-on:click="stopMetronome(bpm);startMetronome(bpm, snd1, snd2)">Start</button>
-        <button id="stopButton" v-on:click="stopMetronome(bpm)">Stop</button>
-
-        <p id="bpmBox">{{ beats }} beats per minute</p> 
+        
+        <div id="beatsBox">
+            Beats per Minute: <input type="text" v-model="bpm" :value="beats"  v-on:keyup.enter="updateMetronome(bpm, snd1, snd2, snd3, snd4)"> 
+            <button id="startButton" v-on:click="updateMetronome(bpm, snd1, snd2)">Start</button>
+            <button id="stopButton" v-on:click="stopMetronome(bpm)">Stop</button>
+        </div>
         <div id="soundBox">
         <!--
-            <select v-for="(item,index) in beatObj" v-model="snd1" v-on:change="changeSound(snd1, snd2)">
+            <select v-for="(item,index) in beatObj" v-model="snd1" v-on:change="updateMetronome(snd1, snd2)">
                 <option v-for="item in sounds" :value="'sounds/' + item +'.wav'">{{ item }}</option>         
             </select>
         -->
             <div class="soundSelector"  :class="{active: counter % 4 == 1 && counter > 8 }">
             <p>1. beat</p>
-                <select v-model="snd1" v-on:change="changeSound(snd1, snd2, snd3, snd4)">
+                <select v-model="snd1" v-on:change="updateMetronome(bpm, snd1, snd2, snd3, snd4)">
                     <option v-for="item in sounds" :value="'sounds/' + item +'.wav'">{{ item }}</option>         
                 </select>
             </div>
             <div class="soundSelector"  :class="{active: counter % 4 == 2 && counter > 8 }">
             <p>2. beat</p>
-                <select v-model="snd2" v-on:change="changeSound(snd1, snd2, snd3, snd4)">
+                <select v-model="snd2" v-on:change="updateMetronome(bpm, snd1, snd2, snd3, snd4)">
                     <option v-for="item in sounds" :value="'sounds/' + item +'.wav'">{{ item }}</option>       
                 </select>
             </div>
             <div class="soundSelector"  :class="{active: counter % 4 == 3 && counter > 8 }">
             <p>3. beat</p>
-                <select v-model="snd3" v-on:change="changeSound(snd1, snd2, snd3, snd4)">
+                <select v-model="snd3" v-on:change="updateMetronome(bpm, snd1, snd2, snd3, snd4)">
                     <option v-for="item in sounds" :value="'sounds/' + item +'.wav'">{{ item }}</option>       
                 </select>
             </div>
             <div class="soundSelector"  :class="{active: counter % 4 == 0 && counter > 8 }">
             <p>4. beat</p>
-                <select v-model="snd4" v-on:change="changeSound(snd1, snd2, snd3, snd4)">
+                <select v-model="snd4" v-on:change="updateMetronome(bpm, snd1, snd2, snd3, snd4)">
                     <option v-for="item in sounds" :value="'sounds/' + item +'.wav'">{{ item }}</option>       
                 </select>
             </div>
@@ -80,6 +81,42 @@ app = new Vue({
     components: {},
     methods: {
         startMetronome: function(bpm, sound1, sound2, sound3, sound4) {
+            var audio = document.querySelector("audio");
+            /* Counting in... */
+            if (this.counter < 8) {
+                if (this.counter < 4 && this.counter % 2 == 0) {
+                    audio = document.querySelector("audio[src='sounds/tink.wav']");
+                } else {
+                    audio = null;
+                }
+                if (this.counter > 3 && this.counter < 8) {
+                    audio = document.querySelector("audio[src='sounds/tink.wav']");
+                }
+                /* Each beats are set */
+            } else if (this.counter % 4 == 0) {
+                audio = document.querySelector("audio[src='" + this.beatObj.sound1 + "']");
+                audio.volume = 1;
+            } else if (this.counter % 4 == 1) {
+                audio = document.querySelector("audio[src='" + this.beatObj.sound2 + "']");
+                audio.volume = 0.8;
+            } else if (this.counter % 4 == 2) {
+                audio = document.querySelector("audio[src='" + this.beatObj.sound3 + "']");
+                audio.volume = 0.8;
+            } else {
+                audio = document.querySelector("audio[src='" + this.beatObj.sound4 + "']");
+                audio.volume = 0.8;
+            }
+            if (audio) {
+                audio.currentTime = 0; //restart sound if .wav is still playing
+                audio.play();
+            }
+
+            this.counter++;
+            this.isPlaying = true;
+            timerObj = setTimeout(this.startMetronome, 60000 / this.beats)
+        },
+
+        updateMetronome: function(bpm, sound1, sound2, sound3, sound4) {
             if (sound1) {
                 this.beatObj.sound1 = sound1;
             }
@@ -101,47 +138,9 @@ app = new Vue({
             } else if (bpm > 19 && bpm < 301) {
                 this.beats = bpm
             }
-            this.isPlaying = true;
-            var audio = document.querySelector("audio");
-            /* Counting in... */
-            if (this.counter < 8) {
-                if (this.counter < 4 && this.counter % 2 == 0) {
-                    audio = document.querySelector("audio[src='sounds/tink.wav']");
-                } else {
-                    audio = null;
-                }
-                if (this.counter > 3 && this.counter < 8) {
-                    audio = document.querySelector("audio[src='sounds/tink.wav']");
-                }
-                /* Beats are set... */
-            } else if (this.counter % 4 == 0) {
-                audio = document.querySelector("audio[src='" + this.beatObj.sound1 + "']");
-                audio.volume = 1;
-            } else if (this.counter % 4 == 1) {
-                audio = document.querySelector("audio[src='" + this.beatObj.sound2 + "']");
-                audio.volume = 0.7;
-            } else if (this.counter % 4 == 2) {
-                audio = document.querySelector("audio[src='" + this.beatObj.sound3 + "']");
-                audio.volume = 0.7;
-            } else {
-                audio = document.querySelector("audio[src='" + this.beatObj.sound4 + "']");
-                audio.volume = 0.7;
+            if (!this.isPlaying) {
+                this.startMetronome(bpm, sound1, sound2, sound3, sound4);
             }
-            if (audio) {
-                audio.currentTime = 0;
-                audio.play();
-            }
-
-            this.counter++;
-            timerObj = setTimeout(this.startMetronome, 60000 / this.beats)
-        },
-
-        changeSound: function(sound1, sound2, sound3, sound4) {
-            console.log(sound1, sound2)
-            this.beatObj.sound1 = sound1;
-            this.beatObj.sound2 = sound2;
-            this.beatObj.sound3 = sound3;
-            this.beatObj.sound4 = sound4;
         },
 
         stopMetronome: function() {
